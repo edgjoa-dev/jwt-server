dd77337e8ee17afa04d043da07a777ab68186326
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-08-22 14:53:11 -0500

    Carga de imagenes en claudinary ok

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -107,10 +107,12 @@ const actualizarImagenCloudinary = async( req, res = response ) => {
     }
 
     //Limpieza de imagenes
-
     if(modelo.img){
         //Borrar img del servidor
-
+        const nombreArr     = modelo.img.split('/');
+        const nombre        = nombreArr[ nombreArr.length - 1 ];
+        const [ public_id ] = nombre.split('.');
+        cloudinary.uploader.destroy( public_id );
     }
 
     const { tempFilePath } = req.files.archivo
